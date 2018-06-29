367e69c2181906123c81ef7fe288a05abd6ccd3f
Arison <784602719@qq.com>
2018-06-29 17:59:56 +0800

    引入antd ui框架。

---
+++ src/example/SocketsManage.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Arison on 2018/6/28.
+ */
+import React from 'react';
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+/**
+ * Created by Arison on 2018/6/28.
+ */
+class SocketsManage extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+
+
+    render(){
+        return <div>
+
+        </div>
+    }
+}
+
+export  default SocketsManage;
--- src/router/basic/Home.js
+++ src/router/basic/Home.js
@@ -9,6 +9,7 @@ import SiginForm from "../../component/SiginForm";
 import JHttpRequet from "../../component/JHttpRequet";
 import BSList from "../../bootstrap/list/BSList";
 import LoginForm from "../../component/LoginForm";
+import SocketsManage from "../../example/SocketsManage";
 /**
  * Created by Arison on 2018/6/13.
  */
@@ -24,6 +25,7 @@ class Home extends React.Component{
                           <li><Link to={`${this.props.match.url}/login`}>登录组件</Link></li>
                           <li><Link to={`${this.props.match.url}/http`}>网络组件</Link></li>
                           <li><Link to={`${this.props.match.url}/bootstrap`}>样式组件</Link></li>
+                          <li><Link to={`${this.props.match.url}/sockets`}>Sockets管理</Link></li>
                       </ul>
                   </div>
                   <div className="col-md-10" style={{border:"1px solid #f4f4f4"}}>
@@ -32,6 +34,7 @@ class Home extends React.Component{
                       <Route path={`${this.props.match.url}/login`} component={LoginForm}/>
                       <Route path={`${this.props.match.url}/http`} component={JHttpRequet}/>
                       <Route path={`${this.props.match.url}/bootstrap`} component={BSList}/>
+                      <Route path={`${this.props.match.url}/sockets`} component={SocketsManage}/>
                   </div>
         </div>
     }
